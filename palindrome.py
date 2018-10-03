8b9871e5717e6dc9790a5b0235281aa14db18d89
yashk2000 <41234408+yashk2000@users.noreply.github.com>
2018-10-03 18:09:17 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -10,7 +10,6 @@ s9 = str(input("enter string 9"))
 s10 = str(input("enter string 10"))
 s11 = str(input("enter string 11"))
 s12 = str(input("enter string 12"))
-#instead of taking so many inputs it's better to take input using an array
 s13 = s1 + s2 + s3
 s14 = s4 + s5 + s6
 s15 = s7 + s8 + s9
@@ -20,4 +19,25 @@ if sa == srev :
     print("\n",sa,"is palindrome")
 else :
     print("\n",sa,"is not palindrome")
+    
+"""better version using lists and taking every group of 3 strings :
 
+
+list= [""] * 12
+for i in range (12):
+    str1=raw_input("enter name:")
+    list[i]=str1
+i=0
+for i in range (10):
+    strr=list[i]+list[1+i]+list[i+2]
+    l=len(strr)-1
+    str2=""
+    while (l>=0):
+       str2+=strr[l]
+       l=l-1
+    if(strr==str2):
+       print "palindrome"
+    else:
+       print "not palindrome"
+       
+       """
