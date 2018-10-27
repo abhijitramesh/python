60ff9dc2777cdafc707e399cab46405a232534e3
abhijitramesh <43090559+abhijitramesh@users.noreply.github.com>
2018-10-27 11:59:04 +0530

    Create bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,13 @@
+a = []
+for i in range(0,10):
+	new_element = int(input())
+	a.append(new_element)
+i = 0
+for i in range(0,10):
+	for j in range(0,9-i):
+		if a[j]>a[j+1]:
+			temp = a[j+1]
+			a[j+1] = a[j]
+			a[j] = temp
+
+print(a)
